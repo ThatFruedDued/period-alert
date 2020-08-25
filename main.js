daf724af8e5ef72f6b561dd691ac8f73ade46a0b
Frued Dued <57722939+ThatFruedDued@users.noreply.github.com>
2020-08-24 21:28:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+let dateArr,
+    nameArr;
+
+if (localStorage.getItem("times") !== undefined) {
+  dateArr = localStorage.getItem("times").split("spl");
+  nameArr = localStorage.getItem("times").split("spl");
+}
+
+get("#add").addEventListener("click", e => {
+  function type() {
+    let result = window.prompt("Period or Event?");
+    if (result.toLowerCase() === "period") {
+      period();
+    }
+  }
+  function period() {
+    let name = window.prompt("Enter period name");
+    let start = window.prompt("Enter period start time (HH:MM in 24 hour time)");
+    let end = window.prompt("Enter period end time (HH:MM in 24 hour time)");
+    if (start.indexOf(":") === -1 || end.indexOf(":") === -1) {
+      period();
+    }
+    else {
+      dateArr.push(start, end);
+    }
+  }
+});
+
 function get(selector) {
   if (selector.startsWith("#")) {
     return document.getElementById(selector.subString(1));
